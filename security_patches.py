674290b8bc8cf67ae3ebe1824eb1d704f9f77ddd
kelanoconnor <oconnor595@gmail.com>
2020-06-30 07:57:15 -0400

    Update security_patches.py

--- security_patches.py
+++ security_patches.py
@@ -1,28 +1,30 @@
-# Date: 7/13/2017
+# Date: 6/30/2020
 # Author: Kelan O'Connor
 
 # This program will check for common Linux server vulnerabilities and attempt to correct them.
 # Conversion from bash to python in progress...
 
+#!/bin/python
 from sys import argv
 import os
 
-echo ("Updating server packages...")
+print ("For Ubuntu use only.")
+print ("Updating server packages...")
 
 
 os.system('TS=$(date +%s) && mv /etc/apt/sources.list /etc/apt/sources.backup.$TS;printf "deb http://us.archive.ubuntu.com/ubuntu precise main universe multiverse\ndeb http://us.archive.ubuntu.com/ubuntu precise-updates main universe\n" > /etc/apt/sources.list;apt-get -y update > /dev/null 2>&1')
 os.system('apt-get install curl > /dev/null 2>&1')
 
-echo "Do you want to update all system packages or just Apache?"
-update = raw_input("y for all, n for just Apache")
+print ("Do you want to update all system packages or just Apache? ")
+update = input("y for all, n for just Apache")
 
 if update == 'y':
     os.system('apt-get -y upgrade') #full server package upgrade
 elif update == 'n':
-    print "OK. Only upgrading Apache software."
+    print ("OK. Only upgrading Apache software.")
     os.system('apt-get -y upgrade apache2')
 else:
-    print "Not a valid response. Please run the script again."
+    print ("Not a valid response. Please run the script again.")
     exit(0)
 
 print ("### Checking /etc/apache2/apache2.conf... ###")
